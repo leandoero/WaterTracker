868f4d9cf9e09cc886ee16dd66a08ab115ee5757
leandoero <seventeen.iinc@gmail.com>
2025-06-06 17:43:47 +0300

    added navigation

--- app/src/main/java/com/example/watertracker/MainActivity.kt
+++ app/src/main/java/com/example/watertracker/MainActivity.kt
@@ -5,11 +5,29 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.watertracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        //Достаётся NavController из NavHostFragment. NavHostFragment всегда содержит NavController, который управляет переходами между фрагментами.
+        val navController = navHostFragment.navController
+
+        //Связывает нижнее меню с навигационным контроллером.
+        binding.bottomNav.setupWithNavController(navController)
+
+
     }
 }
