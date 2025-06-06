444744afa37456549ad104b7a5e77c3d089dc30b
leandoero <seventeen.iinc@gmail.com>
2025-06-06 21:04:38 +0300

    edit textView id

--- app/src/main/java/com/example/watertracker/screens/MainFragment.kt
+++ app/src/main/java/com/example/watertracker/screens/MainFragment.kt
@@ -2,15 +2,22 @@ package com.example.watertracker.screens
 
 import android.content.Context
 import android.os.Bundle
+import android.os.Looper
 import android.view.View
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.watertracker.R
 import com.example.watertracker.databinding.FragmentMainBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.logging.Handler
 
 class MainFragment : Fragment(R.layout.fragment_main) {
 
+    private val timeFormat = SimpleDateFormat("HH:mm", Locale.getDefault())
+    private val currentTime = timeFormat.format(Date())
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
@@ -18,13 +25,11 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         _binding = FragmentMainBinding.bind(view)
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.localTime.text = currentTime
         binding.addButton.setOnClickListener {
             val toast = Toast.makeText(requireContext(), "ASDADA", Toast.LENGTH_SHORT)
             toast.show()
         }
-
-
 
     }
 }
