72025d9145bd507920429bbcc8e3cb849d06a917
leandoero <seventeen.iinc@gmail.com>
2025-06-09 17:13:18 +0300

    changing the color of the icon when selecting a user. screenshots added. creating a README.md file

--- app/src/main/java/com/example/watertracker/screens/MainFragment.kt
+++ app/src/main/java/com/example/watertracker/screens/MainFragment.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.view.View
 import android.widget.Spinner
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.lifecycleScope
@@ -29,6 +30,7 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     private val binding get() = _binding!!
     private val handler = android.os.Handler(Looper.getMainLooper())
     lateinit var db: MainDb
+    var goalReached = false
     val dateOnly = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(Date())
 
     private val updateTimeRunnable =
@@ -54,8 +56,28 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         // Сохранять при изменении
         slider.addOnChangeListener { _, value, _ ->
             prefs.edit().putFloat("slider_value", value).apply()
+            setColor()
         }
     }
+
+    private fun setColor() {
+        val goal = binding.slider.value * 1000
+            if(goal <= 0f) {
+                // Если цель 0 или меньше — иконка серая
+                binding.iconImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.ban_main_color))
+                goalReached = false
+            }
+            else if(currentSumVolume >= goal)  {
+                // Цель достигнута — иконка серая (можно красной или другой)
+                binding.iconImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.ban_main_color))
+                goalReached = true
+            }
+            else  {
+                // Иначе иконка голубая — цель не достигнута
+                binding.iconImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.main_color))
+                goalReached = false
+            }
+        }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentMainBinding.bind(view)
@@ -69,19 +91,22 @@ class MainFragment : Fragment(R.layout.fragment_main) {
         * */
         handler.post(updateTimeRunnable)
         saveSlider()
+        setColor()
 
         lifecycleScope.launch {
             db.getDao().getSumVolumeByDate(dateOnly).collect { value ->
                 currentSumVolume = value ?: 0f
+                setColor()
             }
         }
 
         binding.addButton.setOnClickListener {
             val goal = binding.slider.value * 1000
             if (binding.slider.value <= 0f) {
-                Toast.makeText(requireContext(), "Set a goal to continue.", Toast.LENGTH_LONG).show()
+                Toast.makeText(requireContext(), "Set a goal to continue.", Toast.LENGTH_SHORT).show()
             } else if (currentSumVolume >= goal) {
-                Toast.makeText(requireContext(), "Goal for the day completed!", Toast.LENGTH_LONG).show()
+                binding.iconImageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.ban_main_color))
+                Toast.makeText(requireContext(), "Goal for the day completed!", Toast.LENGTH_SHORT).show()
             } else {
                 val selected = binding.spinnerLiters.selectedItem.toString()
                 val numberOnly = selected.filter { it.isDigit() || it == '.' }
@@ -91,13 +116,8 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
                 lifecycleScope.launch(Dispatchers.IO) {
                     db.getDao().insertItem(item)
-                    withContext(Dispatchers.Main){
-                        Toast.makeText(requireContext(), "Added", Toast.LENGTH_SHORT).show()
-                    }
-
                 }
             }
         }
-
     }
 }
